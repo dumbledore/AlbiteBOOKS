d7f9431e9ffa8a5b4e93037530fb0e7a8421fcf6
Svetlin Ankov <galileostudios@gmail.com>
2010-12-18 22:37:42 +0200

    Added epub & jar mime types to mongrel's config

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,12 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 Mime::Type.register_alias 'application/xhtml+xml', :mobile
+
+#Rack::Mime::MIME_TYPES.merge!({
+#  ".jad" => "text/vnd.sun.j2me.app-descriptor",
+#  ".jar" => "application/java-archive",
+#  ".epub" => "application/epub+zip"
+#  })
+
+Mongrel::DirHandler::add_mime_type('.epub', 'application/epub+zip')
+Mongrel::DirHandler::add_mime_type('.jad', 'text/vnd.sun.j2me.app-descriptor')
